b8c2dc16312d17c60ab34f287d8546126bd7b65c
Benjamin O <jeep70cp@gmail.com>
2023-08-27 21:51:11 -0400

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
+const dateFilterPlugin = require('nunjucks-date-filter');
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+  // Copy assets
+  eleventyConfig.addPassthroughCopy("assets/img/**/*")
+  eleventyConfig.addPassthroughCopy("assets/js/**/*")
+  
+  // Copy favicon stuff
+  eleventyConfig.addPassthroughCopy("*.png")
+  eleventyConfig.addPassthroughCopy("*.svg")
+  eleventyConfig.addPassthroughCopy("*.ico")
+  eleventyConfig.addPassthroughCopy("site.webmanifest")
+  eleventyConfig.addPassthroughCopy("browserconfig.xml")
+
+  eleventyConfig.addFilter("toISO", (dateObj) => {
+    return dateObj.toISOString().substring(0, 10);
+  });
+};
---
+++ courses.md
@@ -0,0 +1,13 @@
+---
+title: CMU Courses - bowad.net
+name: CMU Courses
+layout: layouts/page.njk
+eleventyNavigation:
+  key: CMU Courses
+  order: 2
+---
+A chronology of the courses I have taken thus far at Carnegie Mellon University.
+
+<!-- Inspired by [Abigale Kim](https://abigalekim.github.io/courses/)'s course reviews. -->
+
+### More coming soon&trade;
